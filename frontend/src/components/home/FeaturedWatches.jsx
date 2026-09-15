81351baef4bd41f8fc61b1f2fcd0158df68af366
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2026-09-16 00:51:20 +0600

    feat: build featured watches section

--- frontend/src/components/home/FeaturedWatches.jsx
+++ frontend/src/components/home/FeaturedWatches.jsx
@@ -1,3 +1,13 @@
 export function FeaturedWatches() {
-  return <div>This is the FeaturedWatches</div>;
+  return (
+    <section>
+      {/* FEATURED WATCH CARD */}
+      <div>
+        {/* WATCH IMAGE */}
+        <img src="/images/watches/5-sports-skx-series-srpd55k1.jpg" alt="" />
+        {/* WATCH NAME  */}
+        <h1>5 sports skx series srpd55k1</h1>
+      </div>
+    </section>
+  );
 }
